063c96d688cca61d4c5b134d8c92aeaa96ae7778
Yaser Almasri <yaserms@outlook.com>
2015-12-07 20:49:49 -0600

    require rqrcode_png and number_to_words

--- lib/m_cfdi.rb
+++ lib/m_cfdi.rb
@@ -1,15 +1,17 @@
-require "m_cfdi/version"
-require "m_cfdi/base"
-require "m_cfdi/addenda"
-require "m_cfdi/address"
-require "m_cfdi/certificate"
-require "m_cfdi/complement"
-require "m_cfdi/concept"
-require "m_cfdi/entity"
-require "m_cfdi/key"
-require "m_cfdi/taxes"
-require "m_cfdi/invoice"
-require "m_cfdi/xml"
+require 'rqrcode_png'
+require 'number_to_words'
+require 'm_cfdi/version'
+require 'm_cfdi/base'
+require 'm_cfdi/addenda'
+require 'm_cfdi/address'
+require 'm_cfdi/certificate'
+require 'm_cfdi/complement'
+require 'm_cfdi/concept'
+require 'm_cfdi/entity'
+require 'm_cfdi/key'
+require 'm_cfdi/taxes'
+require 'm_cfdi/invoice'
+require 'm_cfdi/xml'
 
 module MCFDI
   # Your code goes here...
