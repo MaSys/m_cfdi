92346bfa6960f0dfdb7d4b6c2fad5ee09f34d09f
Yaser Almasri <yaser@masys.co>
2016-06-14 16:31:22 -0500

    add payments way catalog

---
+++ lib/m_cfdi/payment_ways.rb
@@ -0,0 +1,8 @@
+module MCFDI
+  def self.payment_ways
+    [
+      ['Pago en una sola exhibicion', 'Pago en una sola exhibicion'],
+      ['Pago en parcialidades', 'Pago en parcialidades']
+    ]
+  end
+end
