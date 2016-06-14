73e1fc03e4283742200f6d8903fb5f0f5c4334d2
Yaser Almasri <yaser@masys.co>
2016-06-14 16:31:06 -0500

    add payment methods 2016 catalog

---
+++ lib/m_cfdi/payment_methods.rb
@@ -0,0 +1,16 @@
+module MCFDI
+  def self.payment_methods
+    [
+      ['Efectivo', 'Efectivo'],
+      ['Cheque', 'Cheque'],
+      ['Transferencia electrónica de fondos', 'Transferencia electrónica de fondos'],
+      ['Tarjetas de crédito', 'Tarjetas de crédito'],
+      ['Monederos electrónicos', 'Monederos electrónicos'],
+      ['Dinero electrónico', 'Dinero electrónico'],
+      ['Vales de despensa', 'Vales de despensa'],
+      ['Tarjeta de Débito', 'Tarjeta de Débito'],
+      ['Tarjeta de Servicio', 'Tarjeta de Servicio'],
+      ['Otros', 'Otros']
+    ]
+  end
+end
